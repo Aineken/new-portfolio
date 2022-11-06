f01990eae3d8a848badc7cb19a48468c7c0001c5
andrzul <aineken93@gmail.com>
2022-11-07 00:38:15 +0100

    one more

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Container } from "./AppStyles";
 import About from "./components/About/About";
 import Achievements from "./components/Achievements/Achievements";
 import Footer from "./components/Footer/Footer";
@@ -6,6 +5,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
 import Technoligies from "./components/Technologies/Technoligies";
+import { Container } from "./styles/GlobalComponents";
 
 function App() {
   return (
--- src/styles/GlobalComponents/index.js
+++ src/styles/GlobalComponents/index.js
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+export const Container = styled.div`
+  max-width: 1280px;
+  width: 100%;
+  margin: auto;
+`;
 
 export const Section = styled.section`
   display: ${(props) => (props.grid ? "grid" : "flex")};
